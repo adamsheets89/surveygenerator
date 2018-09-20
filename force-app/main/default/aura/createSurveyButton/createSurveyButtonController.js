f0357675f86690baa47c3739eab50689ea9d77df
adamsheets89 <adamsheets89@gmail.com>
2018-09-20 12:19:48 -0500

    survey info saves in q a objects

--- force-app/main/default/aura/createSurveyButton/createSurveyButtonController.js
+++ force-app/main/default/aura/createSurveyButton/createSurveyButtonController.js
@@ -1,21 +1,12 @@
 ({
     clickCreateSurvey : function(component, event, helper) {
-        console.log('line 3 id ', component.get('v.recordId'));
-        var action = component.get('c.getSurveyTemplate');
-        action.setParams({
-            recordId: component.get("v.recordId")
-        });
-        action.setCallback(this, function(response) {
-            var parsedRes = JSON.parse(response.getReturnValue());
-            var results = parsedRes.results.templateInfo;
-            if (parsedRes.isSuccess) {
-                component.set('v.surveyName', results.name);
-                var numberOfQuestions = results.Number_of_Questions__c;
-                var answersPerQuestions = results.Answers_Per_Question__c;
-            } else {
-                //error handle
+        var evt = $A.get("e.force:navigateToComponent");
+        evt.setParams({
+            componentDef: "c:surveyTemplate",
+            componentAttributes: {
+                recordId: component.get("v.recordId")
             }
         });
-        $A.enqueueAction(action);
+        evt.fire();
     }
 })

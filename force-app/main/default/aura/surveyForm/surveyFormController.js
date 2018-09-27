c2c2905703ba4391804198af43cbd9b87d003291
adamsheets89 <adamsheets89@gmail.com>
2018-09-27 17:28:05 -0500

    user survey history complete

--- force-app/main/default/aura/surveyForm/surveyFormController.js
+++ force-app/main/default/aura/surveyForm/surveyFormController.js
@@ -20,26 +20,30 @@
         var surveyResults = component.get('v.questionswithAnswers');
         var recordId = component.get('v.recordId');
         var surveyResponses = [];
+        var numberOfQuestions = 0;
 
         for (var i=0; i<surveyResults.length; i++) {
+            var selectedAnswers = [];
             var response = {
                 question: surveyResults[i].Name__c,
-                answer: '',
+                answer: selectedAnswers,
                 sequence: surveyResults[i].Question_Number__c
             };
+            numberOfQuestions++;
             var answers = surveyResults[i].Answers__r.records
             for (var j=0; j<answers.length; j++) {
                 if (answers[j].isChecked === true) {
-                    response.answer = answers[j].Name__c;
+                    selectedAnswers.push(answers[j].Name__c);
                 } 
             }
             surveyResponses.push(response)
         }
-
+        console.log('line 39 ', numberOfQuestions);
         var params = {
             surveyResponses : surveyResponses,
             surveyName : component.get('v.surveyName'),
-            recordId : recordId
+            recordId : recordId,
+            numberOfQuestions: numberOfQuestions
         };
         var action = component.get('c.saveSurveyResponse');
         action.setParams({
--- force-app/main/default/aura/userHistory/userHistoryController.js
+++ force-app/main/default/aura/userHistory/userHistoryController.js
@@ -3,12 +3,19 @@
         var action = component.get('c.getUserHistory');
         action.setCallback(this, function(response){
             var parsedRes = JSON.parse(response.getReturnValue());
+            var userSurveyList = parsedRes.results.userSurveyList;
             if (parsedRes.isSuccess){
-                component.set('v.surveyHistory', parsedRes.results.userSurveyList);
+                if (!$A.util.isEmpty(userSurveyList)) {
+                    component.set('v.surveyHistory', userSurveyList);
+                    component.set('v.user', userSurveyList[0].User__r.Name);
+                } else {
+                    //show error toast
+                    console.log('EMPTY');
+                }
             } else {
                 console.log(parsedRes.error)
             }
         });
         $A.enqueueAction(action);
-    },
+    }
 })
